698bb8f993774aa60b67417bcda2030ef7f03f04
Eric Daniel Fournier <edf@wifi-131-179-49-252.host.ucla.edu>
2018-02-12 09:36:05 -0800

    Tab fix

--- pkg/types.go
+++ pkg/types.go
@@ -1,37 +1,37 @@
 package prioritizationTool
 
 import (
-    "sync"
+	"sync"
 
-    "gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/mat"
 )
 
 // Profile map
 type ProfileMap struct {
-    sync.Map                            // concurrent map structure
+	sync.Map // concurrent map structure
 }
 
 // Profile type
-type Profile struct{
-    ProfileUsetype      string          // profile usetype
-    HourlyFraction      *mat.Dense      // hourly fraction of supply or demand, unitless
+type Profile struct {
+	ProfileUsetype string     // profile usetype
+	HourlyFraction *mat.Dense // hourly fraction of supply or demand, unitless
 }
 
 // Parcel type
 type Parcel struct {
-	ParcelID            string          // parcel id
-    ParcelUsetype       string          // parcel usetype category 
-	CircuitGroupID      string          // circuit group id
-	AnnualSupply        float64         // annual rooftop solar output energy supply in kWh
-	AnnualDemand        float64         // annual building account energy demand in kWh
+	ParcelID       string  // parcel id
+	ParcelUsetype  string  // parcel usetype category
+	CircuitGroupID string  // circuit group id
+	AnnualSupply   float64 // annual rooftop solar output energy supply in kWh
+	AnnualDemand   float64 // annual building account energy demand in kWh
 }
 
 // CircuitGroup type
 type CircuitGroup struct {
-	CircuitGroupID      string          // circuit group id
-	ParcelCount         int             // parcel count
-	Parcels             chan *Parcel    // collection of parcels within the cg
-	HourlyNetSupply     *mat.Dense      // annual hourly net supply
-	AnnualNetSupply     float64         // annual total net grid exports
-	MaxHourlyNetSupply  float64         // net grid exports in worst case hour
+	CircuitGroupID     string       // circuit group id
+	ParcelCount        int          // parcel count
+	Parcels            chan *Parcel // collection of parcels within the cg
+	HourlyNetSupply    *mat.Dense   // annual hourly net supply
+	AnnualNetSupply    float64      // annual total net grid exports
+	MaxHourlyNetSupply float64      // net grid exports in worst case hour
 }
