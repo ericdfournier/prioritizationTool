c33720e3ba9dbdfe5b0c1a23e675c03459e34e82
ericdfournier <me@ericdfournier.com>
2018-02-12 16:44:23 -0800

    Commented constant declarations

--- pkg/const.go
+++ pkg/const.go
@@ -1,5 +1,5 @@
 package prioritizationTool
 
 // set global constants
-const   hrs     int = 8760
-const   cnt     int = 1752
+const   hrs     int = 8760  // total number of hours in the average year
+const   cnt     int = 1752  // total number of circuit groups in the input dataset
